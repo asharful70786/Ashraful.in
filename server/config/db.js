8ef1e0a3315c7befebb430ec9a65fb94bc49d199
Ashraful <ashrafulmomin7076@gmail.com>
2025-06-15 01:29:12 +0530

    mongoDb Atlas Add

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 async function connectDb() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/ashraful");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("DB connected successfully");
   } catch (error) {
     console.log(error);
