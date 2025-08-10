1cf560ca8c9f1153e06b40f0843a5cf8f162e163
Ashraful momin <ashrafulmomin7076@gmail.com>
2025-08-11 02:29:38 +0530

    Database Path fixed

--- server/config/db.js
+++ server/config/db.js
@@ -3,17 +3,19 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-async function connectDb() {
+const connectDb = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined in environment variables");
+    }
     await mongoose.connect(process.env.MONGO_URI, {
       dbName: "App",
     });
-    console.log(" DB connected successfully");
+    console.log(" MongoDB connected successfully");
   } catch (error) {
-    console.error("MongoDB connection failed:", error);
-    process.exit(1);
+    console.error(" MongoDB connection failed:", error.message);
   }
-}
+};
 
 
 process.on("uncaughtException", (error) => {
